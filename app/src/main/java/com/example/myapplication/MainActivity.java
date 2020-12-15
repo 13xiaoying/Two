8167f0e1dc799e42b47df9f18852bacbad52cb78
lhw-0717 <lhw9807177815@163.com>
2020-12-15 19:48:23 +0800

    dfghjk

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 		//信息嘻嘻嘻嘻嘻嘻爱
+		//电饭锅和金额
     }
 }
